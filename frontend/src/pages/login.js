54b8b001ac4512e2ace9cadf701e472d4d5e6a2a
Sakaar Sen <52592149+Sakaar-Sen@users.noreply.github.com>
2024-04-04 14:39:52 +0530

    e

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 import styles from "../styles/login.module.css";
 
 export default function Login() {
@@ -6,28 +8,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [userStatus, setUserStatus] = useState("");
-
-  const checkMyStatus = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:5000/api/me", {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-        },
-      });
-      if (response.ok) {
-        const data = await response.json();
-        // Process the data received
-        console.log(data);
-        setUserStatus(data.sub);
-      } else {
-        // Handle errors
-        console.error("Failed to fetch user information");
-      }
-    } catch (error) {
-      console.error("Error fetching user information:", error);
-    }
-  };
+  const router = useRouter();
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -37,20 +18,14 @@ export default function Login() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          username,
-          password,
-        }),
+        body: JSON.stringify({ username, password }),
       });
-
       if (response.ok) {
         const data = await response.json();
         // set localStorage jwt
-
         localStorage.setItem("jwt", data.jwt);
-
-        // Handle successful login
-        // For example, redirect to another page
+        // Redirect to the home page
+        router.push("/");
       } else {
         // Handle login failure
         setError("Invalid username or password");
@@ -61,52 +36,75 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <button onClick={checkMyStatus}>Check</button>
-      <h1>Status: {userStatus}</h1>
-      <form
-        onSubmit={handleLogin}
-        className="relative space-y-3 rounded-md bg-white p-6 shadow-xl lg:p-10 border border-gray-100 ml-[32rem] mr-[32rem]"
-      >
-        <h1 className="text-xl font-semibold lg:text-2xl">Login</h1>
-        <p className="pb-4 text-gray-500">Sign in to access your account</p>
-
-        <div>
-          <label htmlFor="username"> Username </label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label htmlFor="password"> Password </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="******"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
-          />
-        </div>
-
-        {error && <div className="text-red-500">{error}</div>}
-
-        <div>
-          <button
-            type="submit"
-            className="mt-5 w-full rounded-md bg-blue-600 p-2 text-center font-semibold text-white outline-none focus:ring"
-          >
-            Get Started
+    <div className="flex justify-center items-center h-screen">
+      <div className="relative">
+        <Link href="/">
+          <button className="absolute top-4 left-4 text-gray-500 hover:text-gray-700 focus:outline-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
           </button>
-        </div>
-      </form>
+        </Link>
+        <form
+          onSubmit={handleLogin}
+          className="space-y-3 rounded-md bg-white p-6 shadow-xl lg:p-10 border border-gray-100"
+        >
+          <h1 className="text-xl font-semibold lg:text-2xl">Login</h1>
+          <p className="pb-4 text-gray-500">Sign in to access your account</p>
+          <div>
+            <label htmlFor="username"> Username </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+            />
+          </div>
+          <div>
+            <label htmlFor="password"> Password </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="******"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="mt-2 h-12 w-full rounded-md bg-gray-100 px-3 outline-none focus:ring"
+            />
+          </div>
+          <div className="text-red-500 text-wrap text-sm" style={{ visibility: 'hidden' }}>
+                Password must be at least 8 characters long, contain at least one
+                number, one symbol, and one letter.
+              </div>
+          {error && <div className="text-red-500">{error}</div>}
+          <div className="flex flex-col items-center">
+            <button
+              type="submit"
+              className="mt-5 w-full rounded-md bg-blue-600 p-2 text-center font-semibold text-white outline-none focus:ring"
+            >
+
+              Login Now!
+            </button>
+            <Link href="/signup" className="mt-4 text-blue-500 hover:text-blue-700">
+              Create an account
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
