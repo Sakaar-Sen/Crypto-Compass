430ee6fe954f93c1f38f230c1a6d97bb94b9e5a0
DKNIGHT41 <118896892+DKNIGHT41@users.noreply.github.com>
2024-04-05 10:41:52 +0530

    e

--- frontend/src/pages/components/ExploreElement.js
+++ frontend/src/pages/components/ExploreElement.js
@@ -1,49 +1,148 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const ExploreElement = () => {
-  let [data, setdata] = useState([]);
+const PriceFeed = () => {
+  const [data, setData] = useState([]);
+  const [selectedColumns, setSelectedColumns] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/api/price/feed");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const fetchedData = await response.json();
+      setData(fetchedData);
+    } catch (error) {
+      setError(error);
+      console.error("Error fetching data:", error);
+    }
+  };
+
   useEffect(() => {
-    axios
-      .get("/api/explore")
-      .then((res) => {
-        setdata(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    fetchData();
   }, []);
 
+  const handleColumnSelect = (column) => {
+    if (selectedColumns.includes(column)) {
+      setSelectedColumns(selectedColumns.filter((c) => c !== column));
+    } else {
+      setSelectedColumns([...selectedColumns, column]);
+    }
+  };
+
+  const allColumns = new Set();
+  if (Array.isArray(data) && data.length > 0) {
+    data.forEach((item) => {
+      Object.keys(item).forEach((column) => allColumns.add(column));
+    });
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div className="z-0 mt-12 grid grid-cols-1 md:grid-cols-3 gap-4 place-items-center  ">
-      {data.map((list, i) => {
-        return (
-          <div key={i}>
-            <div className="h-52 w-64 backdrop-blur font-bold border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f] mb-12 z-0 relative">
-              <div className="p-4 text-white z-0">
-                <p>{list.index}</p>
-                <p>{list.price}</p>
-                <div className=" mt-2 flex">
-                  <div className="mr-6   flex-column">
-                    <p className=" text-slate-300">Change 1d </p>
-                    <p className=" text-slate-300">Change 1h</p>
-                    <p className=" text-slate-300">Volume</p>
-                    <p className=" text-slate-300">Market Cap</p>
-                  </div>
-                  <div className="flex-column">
-                    <p className=" text-slate-300">{list["Change 1d"]}</p>
-                    <p className=" text-slate-300">{list["Change 1h"]} </p>
-                    <p className=" text-slate-300">{list["Volume 1d"]} </p>
-                    <p className=" text-slate-300">{list["Market Cap"]} </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+    <div className="min-h-screen bg-gray-100 text-black">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="py-4">
+          <h1 className="text-3xl font-bold">Price Feed</h1>
+        </div>
+        <div className="overflow-x-auto shadow-neon">
+          {data.length > 0 && (
+            <table className="min-w-full divide-y divide-gray-700">
+              <thead>
+                <tr>
+                  {Array.from(allColumns).map((column) => (
+                    <th
+                      key={column}
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                    >
+                      {column}
+                    </th>
+                  ))}
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
+                  >
+                    <div className="relative inline-block">
+                      <button
+                        type="button"
+                        className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                        id="options-menu"
+                        aria-haspopup="true"
+                        aria-expanded="true"
+                      >
+                        Options
+                        <svg
+                          className="-mr-1 ml-2 h-5 w-5"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </button>
+                      <div
+                        className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5"
+                        role="menu"
+                        aria-orientation="vertical"
+                        aria-labelledby="options-menu"
+                      >
+                        <div className="py-1" role="none">
+                          {Array.from(allColumns).map((column) => (
+                            <button
+                              key={column}
+                              type="button"
+                              className={`${
+                                selectedColumns.includes(column)
+                                  ? "bg-gray-100 text-gray-900"
+                                  : "text-gray-700"
+                              } block w-full text-left px-4 py-2 text-sm`}
+                              role="menuitem"
+                              onClick={() => handleColumnSelect(column)}
+                            >
+                              {selectedColumns.includes(column) ? "✓ " : ""}
+                              {column}
+                            </button>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-700">
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    {Array.from(allColumns)
+                      .filter(
+                        (column) =>
+                          selectedColumns.includes(column) || column === "index"
+                      )
+                      .map((column) => (
+                        <td
+                          key={column}
+                          className="px-6 py-4 whitespace-nowrap"
+                        >
+                          {item[column] ?? "-"}
+                        </td>
+                      ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default ExploreElement;
+export default PriceFeed;
